f4dedac97f32f913bf636dbae87c71da66a54441
Il socio <simone.fumagalli@mip.polimi.it>
2018-02-08 12:35:01 +0100

    fix

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default Form extends React.Component {
-  constructor(props){
-    super(props)
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
       question: "",
       options: [],
       output: [],
       progress: 0
-    }
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  render(){
-    return (<div>ciao</div>);
+  render() {
+    return <div>ciao</div>;
   }
-} 
+}
