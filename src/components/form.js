6238c8574384d769c656ea2d155485fea716ba47
Il socio <simone.fumagalli@mip.polimi.it>
2018-02-08 11:25:50 +0100

    Initial commit

---
+++ src/components/form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Search } from "semantic-ui-react";
+
+export default Form extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      question: "",
+      options: [],
+      output: [],
+      progress: 0
+    }
+  }
+
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+
+  render(){
+    return ();
+  }
+
+
+} 
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render } from "react-dom";
+import Form from "./form";
+import "semantic-ui-css/semantic.min.css";
+import { Container, Menu, Input, Image } from "semantic-ui-react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: "home"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Menu secondary>
+            <Menu.Item onClick={this.handleItemClick}>
+              <Image
+                src="https://react.semantic-ui.com/logo.png"
+                size="mini"
+                circular
+              />
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item name="Help" onClick={this.handleItemClick} />
+            </Menu.Menu>
+          </Menu>
+        </Container>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
